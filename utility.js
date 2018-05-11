399ee3f439f6f2b22d6eebf8a18562cb0281f7fe
TheoLeeCJ <theodorelcj@gmail.com>
2018-05-11 17:20:26 +0800

    Utilities Added

--- utility.js
+++ utility.js
@@ -1,19 +1,21 @@
-function Snackbar(text) {
-	document.getElementById("Snackbar-Text").innerHTML = text;
-
+function CloseSnackbar() {
 	var a = document.getElementById("Snackbar");
-	a.style.opacity = 0;
+	a.style.opacity = 1.0;
 	a.style.right = "20px";
 	a.style.top = "20px";
 
-	var c = 0;
+	var c = 1.0;
 
 	var b = setInterval(function() {
-		c = c + 0.02;
+		c = c - 0.02;
 		a.style.opacity = c;
 	}, 10);
 
 	setTimeout(function() {
 		clearInterval(b);
 	}, 500);
+}
+
+function Snackbar(text, clicked = function() {}) {
+	
 }
